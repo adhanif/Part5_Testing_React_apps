610cd09cf4a063e3e1739bbab457f1a927ae0254
ad.hanif <ad.hanif@outlook.com>
2023-09-16 14:46:35 +0200

    5.18: bloglist end to end testing, step2 completed

--- bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,10 +1,35 @@
 describe("Blog app", function () {
   beforeEach(function () {
     cy.request("POST", "http://localhost:3003/api/testing/reset");
+    const user = {
+      name: "mluukkai",
+      username: "mluukkai",
+      password: "salainen",
+    };
+    cy.request("POST", "http://localhost:3003/api/users", user);
     cy.visit("http://localhost:5173");
   });
 
   it("Login form is shown", function () {
-    // ...
+    cy.visit("http://localhost:5173");
+    cy.contains("login");
+  });
+
+  describe("Login", function () {
+    it("succeeds with correct credentials", function () {
+      cy.contains("login");
+      cy.get("#username").type("mluukkai");
+      cy.get("#password").type("salainen");
+      cy.contains("login").click();
+      cy.contains("mluukkai is logged in");
+    });
+
+    it("fails with wrong credentials", function () {
+      cy.contains("login");
+      cy.get("#username").type("mluukkai2");
+      cy.get("#password").type("salainen");
+      cy.contains("login").click();
+      cy.contains('wrong username or password');
+    });
   });
 });
