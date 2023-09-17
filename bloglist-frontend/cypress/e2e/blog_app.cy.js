f0bfc2d0ba25bc4fc18a6998521dd3b27d20d383
ad.hanif <ad.hanif@outlook.com>
2023-09-17 23:15:34 +0200

    5.19: bloglist end to end testing, step3 completed

--- bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,13 +1,13 @@
 describe("Blog app", function () {
   beforeEach(function () {
-    cy.request("POST", "http://localhost:3003/api/testing/reset");
+    cy.request("POST", `${Cypress.env("BACKEND")}/testing/reset`);
     const user = {
       name: "mluukkai",
       username: "mluukkai",
       password: "salainen",
     };
-    cy.request("POST", "http://localhost:3003/api/users", user);
-    cy.visit("http://localhost:5173");
+    cy.request("POST", `${Cypress.env("BACKEND")}/users`, user);
+    cy.visit("");
   });
 
   it("Login form is shown", function () {
@@ -17,19 +17,38 @@ describe("Blog app", function () {
 
   describe("Login", function () {
     it("succeeds with correct credentials", function () {
-      cy.contains("login");
-      cy.get("#username").type("mluukkai");
-      cy.get("#password").type("salainen");
-      cy.contains("login").click();
+      cy.login({ username: "mluukkai", password: "salainen" });
       cy.contains("mluukkai is logged in");
     });
 
     it("fails with wrong credentials", function () {
-      cy.contains("login");
-      cy.get("#username").type("mluukkai2");
-      cy.get("#password").type("salainen");
       cy.contains("login").click();
-      cy.contains('wrong username or password');
+      cy.get("#username").type("mluukkai2");
+      cy.get("#password").type("wrong");
+      cy.contains("login").click();
+      cy.get(".error")
+        .should("contain", "wrong username or password")
+        .and("have.css", "color", "rgb(255, 0, 0)")
+        .and("have.css", "border-style", "solid");
+      cy.get("html").should("not.contain", "mluukkai2 logged in");
+    });
+  });
+
+  describe("When logged in", function () {
+    beforeEach(function () {
+      cy.login({ username: "mluukkai", password: "salainen" });
+    });
+    it("A new blog can be created", function () {
+      cy.wait(2000);
+      cy.get("html").should("contain", "mluukkai is logged in");
+
+      cy.createBlog({
+        title: "CYPRES",
+        author: "CYPRES",
+        likes: "100",
+        url: "CYPRES",
+      });
+      cy.get("html").should("contain", "CYPRES CYPRES");
     });
   });
 });
